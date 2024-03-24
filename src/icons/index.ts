2a62989f96b46a20ee9587a8418746cd82b3c201
Neel <neelp0980@gmail.com>
2024-03-24 17:49:49 +0530

    feat: graph integration

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -2,5 +2,6 @@ import InfoIcon from './Info';
 import ChevronLeft from './ChevronLeft';
 import ChevronRight from './ChevronRight';
 import UploadIcon from './Upload';
+import FarcasterIcon from './Farcaster';
 
-export {InfoIcon, ChevronLeft, ChevronRight, UploadIcon};
+export {InfoIcon, ChevronLeft, ChevronRight, UploadIcon, FarcasterIcon};
