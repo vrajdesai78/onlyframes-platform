e534a605ff01eb6fea22bef6b1af9a62602e5711
Neel <neelp0980@gmail.com>
2024-03-24 20:15:21 +0530

    feat: my products

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,9 @@
 import Navbar from './shared/Navbar';
 import StatsCard from './products/card';
-import ProductTable from './products/table';
 import AddBtn from './products/create';
 import Card from './shared/card';
 import Checkbox from './form/checkbox';
 import Input from './form/input';
 import Upload from './form/upload';
 
-export {Navbar, StatsCard, ProductTable, AddBtn, Card, Checkbox, Input, Upload};
+export {Navbar, StatsCard, AddBtn, Card, Checkbox, Input, Upload};
--- utils/constants.ts
+++
@@ -1,5 +0,0 @@
-// deployed on base sepolia
-export const podsContractAddress = '0xccc8aFf496bCae6a05114dB4c27279d8b6D0942A';
-export const peasContractAddressTest = '0x43B52b1D7524A4783b70AC28D63f08a889E79e95';
-
-const rpcURL = 'https://rpc.ankr.com/base_sepolia';
